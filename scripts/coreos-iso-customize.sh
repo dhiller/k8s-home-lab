b8daadd385d54f2247524b0e7d2f933848a19fe9
Daniel Hiller <daniel.hiller.1972@gmail.com>
2023-10-30 18:41:29 +0100

    Rework ignition and iso generation
    Generate ign and iso per node, include the basic configuration.
    This can be reworked later such that we will have a control-plane
    and worker base and only modify the /etc/hostname in the target
    config.

--- scripts/coreos-iso-customize.sh
+++ scripts/coreos-iso-customize.sh
@@ -1,16 +1,26 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 set -ex
 
 rm -f fcos-*.iso
 
-coreos-installer iso customize \
-    --dest-device /dev/sda \
-    --dest-ignition bootstrap/fcos/fcos-control-plane.ign \
-    -o fcos-control-plane.iso \
-    /home/dhiller/Downloads/fedora-coreos-38.20231002.3.1-live.x86_64.iso
+work_dir="$(pwd)/bootstrap/fcos"
+iso_base="/home/dhiller/Downloads/fedora-coreos-38.20231002.3.1-live.x86_64.iso"
 
-coreos-installer iso customize \
-    --dest-device /dev/sda \
-    --dest-ignition bootstrap/fcos/fcos-worker.ign \
-    -o fcos-worker.iso \
-    /home/dhiller/Downloads/fedora-coreos-38.20231002.3.1-live.x86_64.iso
+(
+    for file in $(find ${work_dir} -type f -name "fcos-control-plane-*.ign" -print); do
+        rm -f ${file/.ign/.iso}
+        coreos-installer iso customize \
+            --dest-device /dev/sda \
+            --dest-ignition ${file} \
+            -o ${file/.ign/.iso} \
+            ${iso_base}
+    done
+    for file in $(find ${work_dir} -type f -name "fcos-worker-*.ign" -print); do
+        rm -f ${file/.ign/.iso}
+        coreos-installer iso customize \
+            --dest-device /dev/sda \
+            --dest-ignition ${file} \
+            -o ${file/.ign/.iso} \
+            ${iso_base}
+    done
+)
--- scripts/generate-ignition.sh
+++ scripts/generate-ignition.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
 
-butane --files-dir /home/dhiller/.ssh bootstrap/fcos/fcos-control-plane.butane.yaml > ./bootstrap/fcos/fcos-control-plane.ign
-butane --files-dir /home/dhiller/.ssh bootstrap/fcos/fcos-worker.butane.yaml > ./bootstrap/fcos/fcos-worker.ign
+set -ex
+
+function exec_butane() {
+    local input_file="$1"
+    local output_file="$2"
+    echo "will write output to ${output_file}"
+    butane \
+        --files-dir ${work_dir} \
+        ${input_file} > ${output_file}
+#    echo "output:"
+#    jq '.' ${output_file} | bat
+}
+
+work_dir="$(pwd)/bootstrap/fcos"
+
+(
+    cd ${work_dir} || exit
+    exec_butane fcos-base.butane.yaml fcos-base.ign
+    for file in $(find ${work_dir} -type f -name "fcos-control-plane-*.butane.yaml" -print); do
+        exec_butane ${file} ${file/.butane.yaml/.ign}
+    done
+    for file in $(find ${work_dir} -type f -name "fcos-worker-*.butane.yaml" -print); do
+        exec_butane ${file} ${file/.butane.yaml/.ign}
+    done
+)
